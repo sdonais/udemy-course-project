70960afe855668cd58f67894959427e7449c13e5
Shelby <tateremail@gmail.com>
2020-11-30 12:30:05 -0600

    updated components

---
+++ recipe-project/src/app/recipes/recipe.model.ts
@@ -0,0 +1,13 @@
+import { normalizeGenFileSuffix } from '@angular/compiler/src/aot/util';
+
+export class Recipe {
+    public name: string;
+    public description: string;
+    public imagePath: string;
+    
+    constructor(name: string, desc: string, imagePath: string) {
+        this.name = name;
+        this.description = desc;
+        this.imagePath = imagePath;
+    }
+}
