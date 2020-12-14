296d8166088fbb42ed8d00b3a49b3cd6241a1ab5
Shelby <tateremail@gmail.com>
2020-12-14 14:39:43 -0600

    updated dropdown directive to close via click anywhere

--- recipe-project/src/app/shared/dropdown.directive.ts
+++ recipe-project/src/app/shared/dropdown.directive.ts
@@ -1,12 +1,26 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+// import { Directive, HostBinding, HostListener } from '@angular/core';
 
+// @Directive({
+//     selector: '[appDropdown]'
+// })
+// export class DropdownDirective {
+//     @HostBinding('class.open') isOpen = false;
+
+//     @HostListener('click') toggleOpen() {
+//         this.isOpen = !this.isOpen;
+//     }
+// } this is what we did in the video. 
+// below, code allowing user to close dropdown by clicking anywhere outside, not just only dropdown menu
+
+import {Directive, ElementRef, HostBinding, HostListener} from '@angular/core';
+ 
 @Directive({
-    selector: '[appDropdown]'
+  selector: '[appDropdown]'
 })
 export class DropdownDirective {
-    @HostBinding('class.open') isOpen = false;
-
-    @HostListener('click') toggleOpen() {
-        this.isOpen = !this.isOpen;
-    }
+  @HostBinding('class.open') isOpen = false;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+  }
+  constructor(private elRef: ElementRef) {}
 }
