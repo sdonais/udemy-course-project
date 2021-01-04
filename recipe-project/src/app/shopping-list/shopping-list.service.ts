1f1e28090d88a207ed0060cee351d3e1c141b3f3
Shelby <tateremail@gmail.com>
2021-01-04 01:27:48 -0600

    implemented subjects

--- recipe-project/src/app/shopping-list/shopping-list.service.ts
+++ recipe-project/src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,10 @@
 import { EventEmitter } from "@angular/core";
+import { Subject } from "rxjs";
+
 import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppingListService {
-    ingredientsChanged = new EventEmitter<Ingredient[]>();
+    ingredientsChanged = new Subject<Ingredient[]>();
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10),
@@ -14,13 +16,13 @@ export class ShoppingListService {
 
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
 
 
     addIngredients(ingredient: Ingredient[]) {
         //this.ingredients.push(...ingredients); //error? this is what the video showed tho 
         this.ingredients.push(...ingredient); // sort of works this way for now
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
 } 
