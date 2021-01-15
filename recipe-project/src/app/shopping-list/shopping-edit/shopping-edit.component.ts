261dd083a459bd9ac7ac52126982988af1f177dd
Shelby <tateremail@gmail.com>
2021-01-14 22:16:12 -0600

    added deletion of shopping list items

--- recipe-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipe-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -33,7 +33,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     );
   }
 
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     if (this.editMode) {
@@ -41,7 +41,19 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     } else {
       this.slService.addIngredient(newIngredient);    
     }
-  } //end onAddItem
+    this.editMode = false; 
+    form.reset();
+  } //end onSubmit
+
+  onClear() {
+    this.slForm.reset();
+    this.editMode = false;
+  }
+
+  onDelete() {
+    this.slService.deleteIngredient(this.editedItemIndex);
+    this.onClear();
+  }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
