a0493d519a9bd29ae041691b3249c78dc997a8d8
Shelby <tateremail@gmail.com>
2021-01-16 13:11:25 -0600

    fixed typo

--- recipe-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,7 @@ import { RecipeService } from '../recipe.service';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit OnDestroy {
+export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
   subscription: Subscription;  
 
